f7f95b143af87a6885327f36111eaf1c703e587a
Amine Triki <amine.triki15@gmail.com>
2026-02-07 23:55:49 +0100

    feat: add new blog page and card component with updated data entries

--- src/data.js
+++ src/data.js
@@ -1,22 +1,24 @@
+const data =
 [
   {
     title: "Converting Your React Vite App into PWA",
-    img: "assets/Converting Your React Vite App into PWA.webp",
+    img: "Converting Your React Vite App into PWA.webp",
   },
   {
     title: "Converting Your React Vite App into PWA",
-    img: "assets/orange fixbox change password & Wi-Fi name.webp",
+    img: "orange fixbox change password & Wi-Fi name.webp",
   },
   {
     title: "create ovhcloud professional email",
-    img: "assets/create ovhcloud professional email.webp",
+    img: "create ovhcloud professional email.webp",
   },
   {
     title: "connection lost. Saving has been disabled until you’re reconnected",
-    img: "assets/connection lost. Saving has been disabled until you’re reconnected.webp",
+    img: "connection lost. Saving has been disabled until you’re reconnected.webp",
   },
   {
     title: "Les fonction logiques : Algèbre de Boole",
-    img: "assets/Les fonction logiques.webp",
+    img: "Les fonction logiques .webp",
   },
 ];
+export default data;

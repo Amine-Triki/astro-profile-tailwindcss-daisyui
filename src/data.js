8a9090f53523b1f457c7c1b5af9c7261d4f9c054
Amine Triki <amine.triki15@gmail.com>
2026-02-07 23:22:12 +0100

    feat: add new assets and data entries for various projects

---
+++ src/data.js
@@ -0,0 +1,22 @@
+[
+  {
+    title: "Converting Your React Vite App into PWA",
+    img: "assets/Converting Your React Vite App into PWA.webp",
+  },
+  {
+    title: "Converting Your React Vite App into PWA",
+    img: "assets/orange fixbox change password & Wi-Fi name.webp",
+  },
+  {
+    title: "create ovhcloud professional email",
+    img: "assets/create ovhcloud professional email.webp",
+  },
+  {
+    title: "connection lost. Saving has been disabled until you’re reconnected",
+    img: "assets/connection lost. Saving has been disabled until you’re reconnected.webp",
+  },
+  {
+    title: "Les fonction logiques : Algèbre de Boole",
+    img: "assets/Les fonction logiques.webp",
+  },
+];
